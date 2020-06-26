c45d77d5005399a1e17fd49cda9ff60f411220fc
PiratePanda978 <kunwar9789@gmail.com>
2020-06-26 06:25:44 +0530

    Modified

---
+++ site/Module_4/js/script.js
@@ -0,0 +1,32 @@
+// /**String Concatenation */
+// var x = "Hello ";
+// x += "World";
+// console.log(x + '!');
+
+// var x=4, y=4;
+// if(x==y){
+//     console.log("x equal to y");
+// }
+// else{
+//     console.log("x not equal to y");
+// }
+
+// var x="4", y=4;
+// if(x===y){
+//     console.log("x equal to y");
+// }
+// else{
+//     console.log("x not equal to y");
+// }
+
+// if(false && 0 && NaN && undefined){
+//     console.log("False");
+// }
+// else{
+//     console.log('All False')
+// }
+
+// if(true && 1 && "Hello"){
+//     console.log("All TRue")
+// }
+
